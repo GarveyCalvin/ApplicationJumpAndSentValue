ad07cd7e185b4574444db3fbc7a578d0f74e69b8
GarveyCalvin <1147626297@qq.com>
2016-08-12 10:07:10 +0800

    添加如何测试Mac自定义Url Scheme的Demo应用

--- MacCustomUrlScheme/MacCustomUrlScheme/AppDelegate.m
+++ MacCustomUrlScheme/MacCustomUrlScheme/AppDelegate.m
@@ -6,6 +6,11 @@
 //  Copyright © 2016 GarveyCalvin. All rights reserved.
 //
 
+/**
+ *  如何通过Safari测试
+ *  只需打开Safari，然后在网址栏中输入 com.gc.custom.url.scheme:// 即可测试
+ */
+
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
